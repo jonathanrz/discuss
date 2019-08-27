976e8041abf582221bb920046e8a21ca0803bb78
Jonathan Zanella <jonathanrafaelz@gmail.com>
2019-08-26 21:21:55 -0300

    user auth

--- config/config.exs
+++ config/config.exs
@@ -14,8 +14,7 @@ config :discuss, Discuss.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "DNcOGZnXuv7X5HChi+gNiipG1mBahzInmxYxDHtTlImilxTh2Km5M0hEjx5NJgDL",
   render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Discuss.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -24,4 +23,10 @@ config :logger, :console,
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
+
+config :ueberauth, Ueberauth, providers: [github: {Ueberauth.Strategy.Github, []}]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "ad0b88a5358385b6ac90",
+  client_secret: "ca6d0e28503b69314db73a17190c024a2d13ebc0"
